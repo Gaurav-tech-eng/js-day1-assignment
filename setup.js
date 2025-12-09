b802c67a7c82c8cb1b27a28082fecfe51f03cb8c
Gaurav Singh <gs9415855@gmail.com>
2025-12-09 02:33:32 +0000

    Added setup.js details

---
+++ setup.js
@@ -0,0 +1,8 @@
+/*
+Steps followed:
+1. Created GitHub account.
+2. Created repository 'js-day1-assignment'.
+3. Opened Codespaces (Virtual Environment).
+4. Verified Node.js installation.
+5. Created intro.js and ran it using node command.
+*/
